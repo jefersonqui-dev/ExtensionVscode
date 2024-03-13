2c1a0c901ae9e7de2bd1c19143f55d2f226a491b
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-13 11:10:57 -0500

    Subiendo Extension a Netlify

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='header__container'>
+        <div className='header__content'>
+          <h1>Sinergy</h1>
+          <div className="card">
+            <p className='header__subtitle'>
+              A minimal, dark blue theme for VS Code
+            </p>
+            <div className='header__image'>
+              <img src="/src/assets/imgext.jpg" alt="#" />
+
+            </div>
+
+          </div>
+          <p className="read-the-docs">
+            VS Code
+          </p>
+        </div >
+      </header>
+      <section className='section__container'>
+        <div >
+          <img src="/src/assets/logo2.png" alt="#" />
+        </div>
+        <p>Made by Jeferson Quiguantar</p>
+      </section>
+    </>
+  )
+}
+
+export default App
