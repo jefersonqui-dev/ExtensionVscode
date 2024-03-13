927d4ad945020ae7870a648e6b299e6aed7d64f4
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-13 16:45:44 -0500

    urls

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
               A minimal, dark blue theme for VS Code
             </p>
             <div className='header__image'>
-              <img src="public\assets\imgext.jpg" alt="header image" />
+              <img href="https://github.com/jefersonqui/ExtensionVscode/blob/main/public/assets/imgext.jpg" alt="header image" />
 
             </div>
 
@@ -28,9 +28,9 @@ function App() {
       </header>
       <section className='section__container'>
         <div >
-          <img src="public\assets\logo2.png" alt="logo" />
+          <img href="https://github.com/jefersonqui/ExtensionVscode/blob/main/public/assets/logo2.png" alt="logo" />
         </div>
-        <p>Made by Jeferson Quiguantar - Code</p>
+        <p>Made by Jeferson Quiguantar</p>
       </section>
     </>
   )
