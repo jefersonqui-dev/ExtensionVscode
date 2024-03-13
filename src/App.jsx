9767a53c24d73bdf422f3bef72ae239ef4bf1593
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-13 17:01:36 -0500

    configvitepublicdir

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,19 @@ function App() {
               A minimal, dark blue theme for VS Code
             </p>
             <div className='header__image'>
-              <img href="https://github.com/jefersonqui/ExtensionVscode/blob/main/public/assets/imgext.jpg" alt="header image" />
+              <img src="public\assets\imgext.jpg" alt="header image" />
 
             </div>
 
           </div>
           <p className="read__the-docs">
-            <a src="https://marketplace.visualstudio.com/items?itemName=jefersonquiguantar.sinergy" target='_blank'>VS Code</a>
+            <a href="https://marketplace.visualstudio.com/items?itemName=jefersonquiguantar.sinergy" target='_blank'>VS Code</a>
           </p>
         </div >
       </header>
       <section className='section__container'>
         <div >
-          <img src="https://github.com/jefersonqui/ExtensionVscode/blob/main/public/assets/logo2.png" alt="logo" />
+          <img src="public\assets\logo2.png" alt="logo" />
         </div>
         <p>Made by Jeferson Quiguantar</p>
       </section>
