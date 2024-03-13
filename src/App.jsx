b4bd5c0ae259704ecf5af0036d3c23ae324780d1
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-13 15:26:44 -0500

    actualizando pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
     <>
       <header className='header__container'>
@@ -14,8 +16,8 @@ function App() {
               A minimal, dark blue theme for VS Code
             </p>
             <div className='header__image'>
-              <img src="/src/assets/imgext.jpg" alt="#" />
-
+              <img src="/src/assets/imgext.jpg" alt="header image" />
+              
             </div>
 
           </div>
@@ -26,7 +28,7 @@ function App() {
       </header>
       <section className='section__container'>
         <div >
-          <img src="/src/assets/logo2.png" alt="#" />
+          <img src="/src/assets/logo2.png" alt="logo" />
         </div>
         <p>Made by Jeferson Quiguantar</p>
       </section>
